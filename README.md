281c704169315bb0e231adaee063c2c10bd89d0b
Joey Hines <joey@ahines.net>
2019-04-08 15:25:29 -0500

    Now using pure-python-adb client for connecting to the target device
    + added comments to better document code
    + added README

---
+++ README.md
@@ -0,0 +1,9 @@
+#Android Time Forensics
+This projects uses ADB to detect time shifts on a target Android device.
+
+##Requirements
+Python 3.6+
+
+[pure-python-adb](https://github.com/Swind/pure-python-adb)
+
+[adb](https://developer.android.com/studio/command-line/adb)
--- android_time_snopper.py
+++ android_time_snopper.py
@@ -1,36 +1,102 @@
-import subprocess
+"""
+Digital Forensics Final Project Spring 2019
+
+Android Time Forensics using ADB
+"""
+
 import datetime
+from adb.client import Client as AdbClient
+import argparse
+import pytz
+
+# Command line arguments
+parser = argparse.ArgumentParser(description="Android Time Forensics Tool")
+parser.add_argument('--adb_port', metavar='p', type=int, nargs='+', help='adb port number', default=5037)
+parser.add_argument('--adb_addr', metavar='a', type=str, nargs='+', help='adb address', default="127.0.0.1")
 
 
 def get_time(time):
+    """
+    Parses a time string of the format "%H:%M:%S,%j,%Y,%z" to get a date
+    :param time: time string
+    :return: datetime object of that string
+    """
     date = datetime.datetime.strptime(time, "%H:%M:%S,%j,%Y,%z\n")
     return date
 
 
+def get_device(client):
+    """
+    Gets the Android Device section from the user
+    :param client: adb client
+    :return: adb device object
+    """
+
+    devices = client.devices()
+    done = False
+    device = None
+
+    # get device selection from the user
+    while not done:
+        if len(devices) == 0:
+            print("No Android Devices Found!")
+            exit(1)
+
+        print("Select a device:")
+        print("Choice\tSerial #")
+        ndx = 0
+        for device in devices:
+            print("%d\t\t%s" % (ndx, device.get_properties()["ro.kernel.androidboot.serialno"]))
+
+        choice = input("\nEnter Choice: ")
+
+        try:
+            device = devices[int(choice)]
+            done = True
+        except Exception as e:
+            print(e)
+            print("Invalid choice, please try again")
+
+    return device
+
+
+def print_properties(device):
+    """
+    Prints a devices properties
+    :param device:
+    :return:
+    """
+    properties = device.get_properties()
+
+    for property in properties:
+        print(property, properties[property])
+
+
 if __name__ == "__main__":
-    print(subprocess.check_output(['adb', 'devices', '-l']).decode('UTF-8'))
+    args = parser.parse_args()
 
-    sys_time = get_time(subprocess.check_output(['date', '-u', '+%H:%M:%S,%j,%Y,%z']).decode('UTF-8'))
-    target_time = get_time(subprocess.check_output(['adb', 'shell', 'date', '-u', '+%H:%M:%S,%j,%Y,%z']).decode('UTF-8'))
+    # Open up ADB client
+    client = AdbClient(args.adb_addr, port=args.adb_port)
 
-    print("System time: %s" % sys_time)
-    print("Android time: %s" % target_time)
+    # Get the device selection from the user
+    device = get_device(client)
+    # print out all device properties
+    properties = device.get_properties()
 
-    if sys_time != target_time:
-        print("\nTime shift found!")
-        print("Time shift: %s\n" % (sys_time - target_time))
+    target_time = get_time(device.shell("date -u +%H:%M:%S,%j,%Y,%z"))
+    system_time = datetime.datetime.now(tz=pytz.utc)
 
-        sys_time = int(subprocess.check_output(['date', '-u', '+%s']).decode('UTF-8'))
-        target_time = int(subprocess.check_output(['adb', 'shell', 'date', '-u', '+%s']).decode('UTF-8'))
-        print("Unix Time shift: %s\n" % (sys_time - target_time))
+    if target_time.timestamp() != system_time.timestamp():
+
+        # print out times
+        print("\nTime Shift Detected on Target Device:")
+        print("Target Time: ", target_time.strftime("%x %X"))
+        print("Workstation Time:", system_time.strftime("%x %X"))
+
+        # get time time shift
+        if system_time > target_time:
+            print("Time Delta: ", system_time - target_time)
+        else:
+            print("Time Delta: ", target_time - system_time)
     else:
-        print("\nTimes match!\n")
-
-
-    time_settings = subprocess.check_output(['adb', 'shell', 'settings', "list", "GLOBAL", "|", "grep", "auto_time"]).decode('UTF-8')
-
-    print("\nAndroid time Settings:")
-    print(time_settings)
-
-
-
+        print("No time shift found")
